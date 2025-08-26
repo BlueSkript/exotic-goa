27e8aba162afca9949455b5099295761a6b9c634
Gokul <lekhwargokul84@gmail.com>
2025-08-26 22:41:33 +0530

    portfolio gallery update

--- src/components/Home/PortfolioGallery.jsx
+++ src/components/Home/PortfolioGallery.jsx
@@ -1,49 +1,62 @@
 import { useState } from "react";
 import styles from "../../styles/Home/PortfolioGallery.module.css";
-import Gimages from "../../../JSONs/Gallery.json";
+import decorGallery from '../../../JSONs/PortfolioDecorGallery.json';
+import hospitalityGallery from '../../../JSONs/PortfolioHospitalityGallery.json';
+import miceGallery from '../../../JSONs/PortfolioMiceGallery.json';
 import PopupGallery from "../PopupGallery";
-const images = [
-  {
-    src: "https://res.cloudinary.com/duh71fcas/image/upload/v1754067237/Exotic%20Data/Home%20Page/Portfolio/IMG-20250528-WA0025.webp",
-    text: "Exotic Goa ",
-  },
-  {
-    src: "https://res.cloudinary.com/duh71fcas/image/upload/v1754067216/Exotic%20Data/Home%20Page/Portfolio/IMG-20250528-WA0023.webp",
-    text: "Exotic Goa hospitality",
-  },
-  {
-    src: "https://res.cloudinary.com/duh71fcas/image/upload/v1754067255/Exotic%20Data/Home%20Page/Portfolio/IMG-20250528-WA0027.webp",
-    text: "Exotic Goa Decor",
-  },
-  {
-    src: "https://res.cloudinary.com/duh71fcas/image/upload/v1754067246/Exotic%20Data/Home%20Page/Portfolio/IMG-20250528-WA0024.webp",
-    text: "Exotic Goa Mice",
-  },
-  {
-    src: "https://res.cloudinary.com/duh71fcas/image/upload/v1754067240/Exotic%20Data/Home%20Page/Portfolio/IMG-20250528-WA0019.webp",
-    text: "Exotic Goa Furniture",
-  },
-];
 
 export default function PortfolioGallery() {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const images = [
+    {
+      src: "https://res.cloudinary.com/duh71fcas/image/upload/v1754067237/Exotic%20Data/Home%20Page/Portfolio/IMG-20250528-WA0025.webp",
+      text: "Exotic Goa",
+      gallery: [...decorGallery, ...hospitalityGallery, ...miceGallery] // all images combined
+    },
+    {
+      src: "https://res.cloudinary.com/duh71fcas/image/upload/v1754067216/Exotic%20Data/Home%20Page/Portfolio/IMG-20250528-WA0023.webp",
+      text: "Exotic Goa Hospitality",
+      gallery: hospitalityGallery
+    },
+    {
+      src: "https://res.cloudinary.com/duh71fcas/image/upload/v1754067255/Exotic%20Data/Home%20Page/Portfolio/IMG-20250528-WA0027.webp",
+      text: "Exotic Goa Decor",
+      gallery: decorGallery
+    },
+    {
+      src: "https://res.cloudinary.com/duh71fcas/image/upload/v1754067246/Exotic%20Data/Home%20Page/Portfolio/IMG-20250528-WA0024.webp",
+      text: "Exotic Goa Mice",
+      gallery: miceGallery
+    },
+    {
+      src: "https://res.cloudinary.com/duh71fcas/image/upload/v1754067240/Exotic%20Data/Home%20Page/Portfolio/IMG-20250528-WA0019.webp",
+      text: "Exotic Goa Furniture",
+      gallery: [], // empty gallery, unclickable
+    },
+  ];
 
-  const closePopup = () => setSelectedImage(null);
+  const [selectedGallery, setSelectedGallery] = useState(null);
+
+  const openGallery = (item) => {
+    if (item.gallery.length > 0) setSelectedGallery(item.gallery);
+  };
+
+  const closePopup = () => setSelectedGallery(null);
 
   return (
     <div className={styles.galleryContainer}>
       {images.map((image, index) => (
         <div
           key={index}
-          className={styles.imageWrapper}
-          onClick={() => setSelectedImage(index)}>
+          className={`${styles.imageWrapper} ${image.gallery.length === 0 ? styles.disabled : ''}`}
+          onClick={() => openGallery(image)}
+        >
           <img src={image.src} alt={image.text} className={styles.image} />
           <div className={styles.imageTextInactive}>{image.text}</div>
         </div>
       ))}
 
-      {selectedImage !== null && (
-       <PopupGallery images={Gimages} onClose={closePopup}></PopupGallery>
+      {selectedGallery && (
+        <PopupGallery images={selectedGallery} onClose={closePopup} />
       )}
     </div>
   );
