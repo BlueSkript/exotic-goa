be55a08d03ede4fb4ca44d4537892919378b9ad7
Gokul <lekhwargokul84@gmail.com>
2025-08-27 00:24:17 +0530

    home mobile changes nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,10 +6,10 @@ import { IoMdClose } from "react-icons/io";
 
 import { motion } from "framer-motion";
 import logo from "/logos/exotic-goa-nav-logo.png";
-import pinkLogo from '/logos/exotic-goa-logo.png'
+import pinkLogo from "/logos/exotic-goa-logo.png";
 
 function NavBar({ bgColor }) {
-  const [menuOpen, setMenuOpen] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => setMenuOpen((prev) => !prev);
   const closeMenu = () => setMenuOpen(false);
@@ -20,27 +20,19 @@ function NavBar({ bgColor }) {
       animate={{ opacity: 1, translateY: 0 }}
       transition={{ duration: 1, ease: "easeInOut" }}
       className={`${bgColor == true ? styles.navAlt : styles.nav}`}>
-      <div className={styles.hamburgerMenuContainer} onClick={toggleMenu}>
+      <div className={styles.hamburgerMenuContainer}>
+        
         {!menuOpen ? (
-          <>
-          <img
-            className={styles.logo}
-            src={logo}
-            alt="exotic-goa-luxury-furniture-logo"
-          />
-          <RxHamburgerMenu className={styles.menuIcon} />
-          </>
+          <RxHamburgerMenu className={styles.menuIcon} onClick={toggleMenu} />
         ) : (
-          <>
-          <img
-            className={styles.logo}
-            src={logo}
-            alt="exotic-goa-luxury-furniture-logo"
-          />
-          <IoMdClose className={styles.menuIcon} />
-          </>
+          <IoMdClose className={styles.menuIcon} onClick={toggleMenu} />
         )}
       </div>
+      <img
+          className={styles.logo}
+          src={logo}
+          alt="exotic-goa-luxury-furniture-logo"
+        />
       <div
         className={`${styles.navlinksContainer} ${
           menuOpen ? styles.showMenu : ""
@@ -51,33 +43,51 @@ function NavBar({ bgColor }) {
           onClick={closeMenu}>
           Home
         </Link>
-        <Link  className={`${bgColor ? styles.linkAlt : styles.link}`} to={"/weddings"} onClick={closeMenu}>
+        <Link
+          className={`${bgColor ? styles.linkAlt : styles.link}`}
+          to={"/weddings"}
+          onClick={closeMenu}>
           Weddings
         </Link>
-        <Link  className={`${bgColor ? styles.linkAlt : styles.link}`} to={"/corporate"} onClick={closeMenu}>
+        <Link
+          className={`${bgColor ? styles.linkAlt : styles.link}`}
+          to={"/corporate"}
+          onClick={closeMenu}>
           Corporate
         </Link>
 
         <Link to={"/"}>
-        {bgColor ? ( <img
-            className={styles.logo}
-            src={pinkLogo}
-            alt="exotic-goa-luxury-furniture-logo"
-          />):( <img
-            className={styles.logo}
-            src={logo}
-            alt="exotic-goa-luxury-furniture-logo"
-          />)}
-       
+          {bgColor ? (
+            <img
+              className={styles.logo}
+              src={pinkLogo}
+              alt="exotic-goa-luxury-furniture-logo"
+            />
+          ) : (
+            <img
+              className={styles.logo}
+              src={logo}
+              alt="exotic-goa-luxury-furniture-logo"
+            />
+          )}
         </Link>
 
-        <Link  className={`${bgColor ? styles.linkAlt : styles.link}`} to={"/our-services"} onClick={closeMenu}>
+        <Link
+          className={`${bgColor ? styles.linkAlt : styles.link}`}
+          to={"/our-services"}
+          onClick={closeMenu}>
           Our Services
         </Link>
-        <Link  className={`${bgColor ? styles.linkAlt : styles.link}`} to={"/blogs"} onClick={closeMenu}>
+        <Link
+          className={`${bgColor ? styles.linkAlt : styles.link}`}
+          to={"/blogs"}
+          onClick={closeMenu}>
           Blogs
         </Link>
-        <Link  className={`${bgColor ? styles.linkAlt : styles.link}`} to={"/contact-us"} onClick={closeMenu}>
+        <Link
+          className={`${bgColor ? styles.linkAlt : styles.link}`}
+          to={"/contact-us"}
+          onClick={closeMenu}>
           Contact Us
         </Link>
       </div>
